53c21740210dbb9e75bee32f71a6f84f9fc5b5d5
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-03-19 18:05:08 +0000

    Implemented booking, user can now book

---
+++ main/java/com/smt/sweettreats/paypark/MakeBooking.java
@@ -0,0 +1,13 @@
+package com.smt.sweettreats.paypark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_booking);
+    }
+}
